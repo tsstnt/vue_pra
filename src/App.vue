4b0731763b1d9f3a8bfcb375b6cf84870b656d62
tsstnt@163.com <52000212+tsstnt@users.noreply.github.com>
2019-11-05 02:24:03 +0800

    无法获取数据

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,22 @@
 <template>
-  <div>
-    <h2 v-if="!repoName">loading...</h2>
-    <p v-else>
-      most star repo is
-      <a :href="repoUrl">{{repoName}}</a>
-    </p>
+  <div class="container">
+    <Search/>
+    <Main/>
   </div>
 </template>
+  
+<script type="text/ecmascript-6">
+import Search from './components/Search'
+import Main from './components/Main'
 
-<script>
-import axios from 'axios'
-export default {
-  data() {
-    return {
-      repoUrl:'',
-      repoName:''
-    };
-  },
-
-  mounted() {
-    // 使用vue-resource发异步ajax请求
-    // this.$http.get('https://api.github.com/search/repositories?q=j&sort=stars')
-    // .then(
-    //   response => {
-    //     const result = response.data
-    //     const {name,html_url} = result.items[0]
-    //     //更新数据
-    //     this.repoName = name
-    //     this.repoUrl = html_url
-    //   },
-    //   error => {
-    //     alert('请求失败')
-    //   }
-    // )
-    // 使用axios发异步ajax请求
-    axios.get('https://api.github.com/search/repositories?q=j&sort=stars')
-    .then(
-      response => {
-        const result = response.data
-        const {name,html_url} = result.items[0]
-        //更新数据
-        this.repoName = name
-        this.repoUrl = html_url
-      },
-      error => {
-        alert('请求失败')
-      }
-    )
-  },
-}
+  export default {
+    components:{
+      Search,
+      Main
+    }
+  }
 </script>
-
-<style>
+  
+<style scoped>
+  
 </style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import App from './App.vue'
 //声明使用vue插件
 Vue.use(VueResource)//给所有的组件对象添加了一个属性$http对象,包含发送ajax的请求方法get()/post()
 
+//创建事件总线并保存
+Vue.prototype.$bus = new Vue()
 new Vue({
   components: {
     App
